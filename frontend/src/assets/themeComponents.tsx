f35a2c683bb0b792552e07d1e4724344d945b94b
Miguel Gonçalves <135244091+miguelgoncalves2024@users.noreply.github.com>
2025-02-27 15:30:09 +0000

    Página de Login 2.0(merge)

--- frontend/src/assets/themeComponents.tsx
+++ frontend/src/assets/themeComponents.tsx
@@ -1,6 +1,37 @@
+import { DisplaySettings } from '@mui/icons-material';
 import { Components } from '@mui/material/styles';
 
 const commonComponents: Components = {
+    MuiCssBaseline: {
+        styleOverrides: {
+            body: {
+                /*Estilo Global do componente Box*/
+                '& .MuiBox-root': {
+                    padding: "16px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                },
+            },
+        },
+    },
+    MuiOutlinedInput: {
+        styleOverrides: {
+            root: {
+                backgroundColor: '#FFFFFF',
+                borderRadius: "15px",
+                '& fieldset': {
+                    border: '1px solid #DDD'
+                },
+                '&:hover fieldset': {
+                    border: '1px solid #1976D2'
+                },
+                '&.Mui-focused fieldset': {
+                    border: '2px solid #1976D2'
+                },
+            },
+        },
+    },
     MuiButton: {
         defaultProps: {
             variant: 'contained',
@@ -12,6 +43,9 @@ const commonComponents: Components = {
             root: {
                 textTransform: 'none',
                 borderRadius: '10px',
+                padding: '10px 20px',
+                fontWeight: 'bold',
+                transition: '0.3s',
             },
         },
     },
@@ -21,6 +55,9 @@ const commonComponents: Components = {
                 backgroundColor: "primary",
                 borderRadius: "10px",
                 padding: "14px",
+                width: "100%",
+                margin: "0 auto",
+                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)'
             },
         },
     },
@@ -32,18 +69,26 @@ const commonComponents: Components = {
         styleOverrides: {
             root: {
                 borderRadius: '0px',
+                marginBottom: '0px',
+                '&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                    borderColor: '#1976D2',
+                },
+                width: '100%'
             },
         },
     },
     MuiTypography: {
-        styleOverrides:{
-            root:{
-                h1: {
-                    fontSize: "24px"
-                }
+        styleOverrides: {
+            h1: {
+                fontSize: '1.5rem',
+                fontWeight: '550',
+            },
+
+            root: {
+                fontSize: '1rem'
             }
-            
-        }
-    }
-}
+        },
+    },
+};
+
 export default commonComponents;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,27 +1,31 @@
-import { Container, Typography, Paper, TextField, Button, Alert, Fade } from '@mui/material';
+import { Container, Typography, Paper, TextField, Button, Alert, Fade, InputAdornment } from '@mui/material';
 import { useRef, useState } from 'react';
 import { useAuth } from '../hooks/AuthContext';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import { Box } from "@mui/material";
+import GoogleIcon from '@mui/icons-material/Google';
+
 
 function LoginPage() {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
     const [isLoading, setLoading] = useState(false);
     const [authError, setAuthError] = useState(false);
-    const { login, loginWithOAuth} = useAuth();
+    const { login, loginWithOAuth } = useAuth();
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault(); // Evita o recarregamento da página
+        event.preventDefault();
         setAuthError(false);
 
         const email = emailRef.current?.value;
         const password = passwordRef.current?.value;
 
-        if (!email || !password) return; // O próprio navegador já lida com a validação do email
+        if (!email || !password) return;
 
         setLoading(true);
 
         try {
-            await new Promise((resolve) => setTimeout(resolve, 1000)); // Simular atraso
+            await new Promise((resolve) => setTimeout(resolve, 1000));
             await login(email, password);
         } catch (error) {
             console.error(error);
@@ -45,22 +49,98 @@ function LoginPage() {
         }
       };    
 
+
+
     return (
-        <Container maxWidth="sm" sx={{ textAlign: 'center', mt: 4 }}>
-            {/* Mensagem de erro com animação */}
+        <Container maxWidth="sm" sx={{ textAlign: 'center', mt: 4, backgroundColor: "background.default", padding: 4, borderRadius: 2 }}>
             <Fade in={authError}>
-                <Alert variant="filled" severity="error" sx={{ mt: 4 }}>
+                <Alert variant="filled" severity="error" sx={{ mt: 2 }}>
                     Email ou Password Inválidos
                 </Alert>
             </Fade>
+            <Box sx={{
+                position: "relative",  
+                marginBottom: 3 // Ajusta conforme necessário
+            }}>
+                <Box sx={{ 
+                    backgroundColor: "primary.main",
+                    borderRadius: "50%", 
+                    width: 70, // Tamanho fixo para garantir que seja circular
+                    height: 70, 
+                    position: "absolute", 
+                    top: "-15px", // Move para cima do Paper
+                    zIndex: 1 // Garante que fique sobre o Paper
+                }}>
+                    <PersonOutlineOutlinedIcon sx={{ fontSize: 48, color: "#FFFFFF" }} />
+                </Box>
+            </Box>
+            <Paper elevation={6} sx={{ 
+                maxWidth: '400px', // Define uma largura máxima
+            }}>
+                <Typography 
+                    variant="h1" 
+                    sx={{ marginBottom: 2, marginTop: 2 }}>
+                    INICIAR SESSÃO
+                </Typography>
 
-            <Paper elevation={6}>
-                <Typography variant="h1" sx={{ marginBottom: 2 }}>Faça Login na sua Conta</Typography>
                 <form onSubmit={handleSubmit}>
-                    <TextField required id="email" label="Email" type="email" inputRef={emailRef} sx={{ mb: 2 }} />
-                    <TextField required id="password" label="Password" type="password" inputRef={passwordRef} sx={{ mb: 2 }} />
-                    <Button fullWidth variant="contained" type="submit" disabled={isLoading}>
-                        Login
+                    <TextField  
+                        required 
+                        id="email" 
+                        label="Email" 
+                        type="email" 
+                        inputRef={emailRef} 
+                    />
+                    <TextField 
+                        required 
+                        id="password" 
+                        label="Password" 
+                        type="password" 
+                        inputRef={passwordRef} 
+                    />
+                    <Box >
+                        <Typography
+                            component="a" 
+                            href="#" 
+                            sx={{ 
+                                color: '#1976D2', 
+                                textDecoration: 'none', 
+                                fontSize: '0.900rem', 
+                                fontWeight: 500, 
+                                cursor: 'pointer',
+                                '&:hover': { textDecoration: 'underline' } 
+                            }}>
+                            Esqueceste-te da tua palavra-passe?
+                        </Typography>
+                    </Box>
+
+                    <Button
+                        type="submit"
+                        disabled={isLoading}
+                        sx={{
+                            background: 'linear-gradient(45deg, #FFA726 30%, #FB8C00 90%)',
+                            color: 'white',
+                            fontWeight: 'bold',
+                            marginTop: 4,
+                            transition: '0.3s',
+                            '&:hover': {
+                                background: 'linear-gradient(45deg, #FB8C00 30%, #FFA726 90%)'
+                            },
+                        }}>
+                        INICIAR SESSÃO
+                    </Button>
+                    <Button
+                        startIcon={<GoogleIcon />}
+                        sx={{
+                            backgroundColor: '#000000',
+                            color: 'white',
+                            transition: '0.3s',
+                            mt: 2,
+                            '&:hover': {
+                                backgroundColor: 'primary.main'
+                            },
+                        }}>
+                        Login com Google
                     </Button>
                 </form>
             </Paper>
