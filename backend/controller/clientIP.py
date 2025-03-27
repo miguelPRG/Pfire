57e759793c37dc5df7b72edee01413476f7a64df
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2025-03-27 10:59:45 +0000

    Atualização de Proteções Abusivas

--- backend/controller/clientIP.py
+++ backend/controller/clientIP.py
@@ -3,8 +3,8 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 
 # Definir o número máximo de requisições e o intervalo de tempo
-LIMIT = 10  # número de requisições permitidas
-TIME_FRAME = 120  # intervalo de tempo (em segundos)
+LIMIT = 5  # número de requisições permitidas
+TIME_FRAME = 60  # intervalo de tempo (em segundos)
 
 # Dicionário em memória para armazenar as requisições por IP
 rate_limiter = {}
