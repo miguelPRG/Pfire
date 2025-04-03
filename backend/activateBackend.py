6c22d8d4df9551073e775d604b339766b8241bfd
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2025-04-03 09:28:39 +0100

    Correção de vulnerabilidade de segurança no python

--- backend/activateBackend.py
+++ backend/activateBackend.py
@@ -2,14 +2,19 @@ import os
 import subprocess
 import sys
 import platform
+import venv
+
+def create_virtual_env():
+    """Cria o ambiente virtual usando o módulo venv."""
+    print("Criando novo ambiente virtual...")
+    venv.create("venv", with_pip=True)
 
 def main():
     # Verifica se a pasta venv existe
     if os.path.isdir("venv"):
         print("Ativando ambiente virtual existente...")
     else:
-        print("Criando novo ambiente virtual...")
-        subprocess.run([sys.executable, "-m", "venv", "venv"], check=True)
+        create_virtual_env()
 
     # Caminho para o interpretador Python dentro do ambiente virtual
     if platform.system() == "Windows":
@@ -18,13 +23,13 @@ def main():
         venv_python = os.path.join("venv", "bin", "python")
 
     # Atualizar pip
-    subprocess.run([venv_python, "-m", "pip", "install", "--upgrade", "pip"], check=True)
+    subprocess.run([venv_python, "-m", "pip", "install", "--upgrade", "pip"], check=True, shell=False)
 
     # Instalar as dependências e remover pacotes não listados em requirements.txt, exceto pip
-    subprocess.run([venv_python, "-m", "pip", "install", "--no-deps", "-r", "requirements.txt"], check=True)
+    subprocess.run([venv_python, "-m", "pip", "install", "--no-deps", "-r", "requirements.txt"], check=True, shell=False)
 
     # Executar o Uvicorn usando o interpretador do ambiente virtual
-    subprocess.run([venv_python, "-m", "uvicorn", "main:app", "--reload"], check=True)
+    subprocess.run([venv_python, "-m", "uvicorn", "main:app", "--reload"], check=True, shell=False)
 
 if __name__ == "__main__":
     main()
