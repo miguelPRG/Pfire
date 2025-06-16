0072d43874a81b9af368e15de5356378c1deb553
Miguel Gonçalves <135244091+miguelgoncalves2024@users.noreply.github.com>
2025-06-16 15:22:46 +0100

    API de criar empresa como utilizador logado

--- backend/routes/Rest/CRUD/empresaCRUD.py
+++ backend/routes/Rest/CRUD/empresaCRUD.py
@@ -1,12 +1,57 @@
 from fastapi import APIRouter, HTTPException, Request
 from apis.recaptchaValidation import validar_recaptcha_token
-from models.empresaModels import EmpresaUpdate
+from models.empresaModels import EmpresaUpdate, EmpresaCreateAsLoggedUser
+from models.userEmpresaModels import UserEmpresaCreate
 from database import empresas_collection, users_empresas_collection
 from bson import ObjectId
 from datetime import datetime
 
 routerEmpresa = APIRouter(prefix="/empresa")
 
+# Criar Empresa
+@routerEmpresa.post("/")
+async def create_empresa(empresa: EmpresaCreateAsLoggedUser, request: Request):
+
+    #Validar o token reCAPTCHA
+    await validar_recaptcha_token(empresa.recaptchaToken, "create")
+
+    # Sacar jwt
+    jwt = getattr(request.state, "jwt", None)
+    logged_user_id = ObjectId(jwt["user_id"])
+
+    empresa_found = await empresas_collection.find_one({"nif": empresa.nif})
+
+    if empresa_found:
+        raise HTTPException(status_code=400, detail="Empresa com este NIF já existe.")
+    
+    data_atual = datetime.now()
+
+    empresa_data = empresa.model_dump(exclude_unset=True)
+    empresa_data["created_by"] = empresa_data["updated_by"] = logged_user_id
+    empresa_data["created_at"] = empresa_data["updated_at"] =  data_atual
+    del empresa_data["recaptchaToken"]
+
+    # Inserir a empresa na coleção de empresas
+    empresa_result = await empresas_collection.insert_one(empresa_data)
+    if not empresa_result.acknowledged:
+        raise HTTPException(status_code=500, detail="Erro ao criar empresa.")
+
+    user_empresa_result = await users_empresas_collection.insert_one(
+        UserEmpresaCreate(
+            user_id=logged_user_id,
+            empresa_id=empresa_result.inserted_id,
+            isAdmin=True,
+            created_by=logged_user_id,
+            created_at=data_atual,
+            updated_by=logged_user_id,
+            updated_at=data_atual
+        ).model_dump(exclude_unset=True)
+    )
+
+    if not user_empresa_result.acknowledged:
+        raise HTTPException(status_code=500, detail="Erro ao associar utilizador à empresa.")
+
+    return {"message": "Empresa Criada com Sucesso!"}
 
 # Atualizar Empresa
 @routerEmpresa.put("/")
