9d330da6b0d1f3b8fc2a065a77b27d64407a6b65
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2026-02-12 22:52:22 +0000

    Pfire 1.5.2 - Atualização do Readme e de dependências

--- frontend/worker.js
+++ frontend/worker.js
@@ -2,7 +2,7 @@ addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
-const BACKEND_URL = "https://pfire-backend-h6xy.onrender.com";
+const BACKEND_URL = "https://pmedsys.com";
 
 export default {
   async fetch(request, env, ctx) {
